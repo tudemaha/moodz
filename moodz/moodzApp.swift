7bff631a01dbff623480e062da75066b8afbfe9e
Tude Maha <gede_mahardika88@yahoo.co.id>
2025-05-21 15:32:15 +0800

    Initial Commit

---
+++ moodz/moodzApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodzApp.swift
+//  moodz
+//
+//  Created by Tude Maha on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct moodzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
