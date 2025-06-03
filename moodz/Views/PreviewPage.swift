8bdb0ecaded2f7b470abe3a7438db6f676abfa53
Tude Maha <gede_mahardika88@yahoo.co.id>
2025-06-03 08:03:33 +0800

    fix: change mood selectio to multiple select and add textfield for place

--- moodz/Views/PreviewPage.swift
+++ moodz/Views/PreviewPage.swift
@@ -1,16 +1,25 @@
 import SwiftUI
 
-// Preview view that accepts an Image as a parameter
 struct PreviewPage: View {
     var selectedImage: UIImage? // Image passed from ContentView
     @State private var rotationAngle: Angle = .zero
     @State private var imageSize: CGSize = CGSize(width: 300, height: 600)
-    @State private var value: Double = 0
-    @State private var backgroundColor: Color = .black
+    
+    @State private var selectedMood: String = "Melancholy"
+    @State private var place = ""
+    
     
     var body: some View {
+        let moods = ["Chill", "Melancholy", "Confident", "Romantinc", "Energetic", "Dreamy"]
+        let moodLayout = [
+            GridItem(.adaptive(minimum: 90)),
+            GridItem(.adaptive(minimum: 90)),
+            GridItem(.adaptive(minimum: 90)),
+        ]
+        
         ZStack(alignment: .top){
             Image("Background_Black")
+            
             VStack{
                 HStack(alignment: .top){
                     NavigationLink(destination: HomePage()) {
@@ -19,14 +28,23 @@ struct PreviewPage: View {
                             .foregroundColor(.white)
                     }.padding()
                     Spacer()
+                    
                     Image("logo_W").resizable()
                         .scaledToFit()
                         .frame(width: 120, height: 80)
+                    
                     Spacer()
-                    RoundedRectangle(cornerRadius: 25).fill(Color.white).frame(width : 80, height: 30).opacity(0)
-                }.padding(.top, 60)
+                    
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.white)
+                        .frame(width : 80, height: 30)
+                        .opacity(0)
+                }
+                .padding(.top, 60)
                 
-                Spacer().frame(height: 30)
+                Spacer()
+                    .frame(height: 30)
+                
                 VStack{
                     if let selectedImage = selectedImage {
                         Image(uiImage: selectedImage) // Use UIImage directly
@@ -43,37 +61,89 @@ struct PreviewPage: View {
                         Text("No image selected")
                             .foregroundColor(.white)
                     }
-                }.frame(height: 450)
+                }
+                .containerRelativeFrame(.vertical) { height, _ in
+                    height * 0.5
+                }
                 
                 VStack{
-                    HStack(){
-                        Text("Masukan Mood dari foto anda : ").font(.custom("HelveticaNeue", size: 18)).foregroundStyle(.white).fontWeight(.semibold)
-                        Spacer()
-                    }.padding(20)
-                    Slider(value: $value, in: 0...100, step: 25)
-                        .padding(.horizontal, 20)
-                        .padding(.top, 10)
-                        .onChange(of: value) { newValue in
-                            updateBackgroundColor()
+                    VStack(spacing: 10) {
+                        HStack {
+                            Text("Select current mood of the image: ")
+                                .font(.custom("HelveticaNeue", size: 18))
+                                .foregroundStyle(.white)
+                                .fontWeight(.semibold)
+                            
+                            Spacer()
                         }
+                        
+                        LazyVGrid(columns: moodLayout) {
+                            ForEach(moods, id: \.self) {mood in
+                                Button {
+                                    selectedMood = mood
+                                } label: {
+                                    Text(mood)
+                                        .padding(.vertical, 8)
+                                        .padding(.horizontal, 10)
+                                        .containerRelativeFrame(.horizontal) { width, _ in
+                                            width * 0.28
+                                        }
+                                        .background(mood == selectedMood ? .P : .white)
+                                        .clipShape(.capsule)
+                                        .font(.custom("HelveticaNeue", size: 16))
+                                        .fontWeight(.semibold)
+                                        .foregroundStyle(mood == selectedMood ? .white : .black)
+                                }
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 20)
                     
-                    Button(action: {
+                    VStack {
+                        HStack(spacing: 10) {
+                            Text("Show the place: ")
+                                .font(.custom("HelveticaNeue", size: 18))
+                                .foregroundStyle(.white)
+                                .fontWeight(.semibold)
+                            
+                            Spacer()
+                        }
+                        
+                        TextField("Place", text: $place)
+                            .frame(height: 35).border(Color.P)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .disableAutocorrection(true)
+                            .background(.white)
+                            .font(.custom("HelveticaNeue", size: 16))
+                            .clipShape(.capsule)
+                        
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Button {
                         // Replace this with your desired action
                         print("Generate Songs Button tapped!")
-                    }) {
+                    } label: {
                         Text("Generate Songs")
                             .font(.custom("HelveticaNeue", size: 16))
                             .fontWeight(.semibold)
                             .foregroundColor(.white)
+                            .containerRelativeFrame(.horizontal) { width, _ in
+                                width * 0.6
+                            }
                             .padding(12)
                             .background(Color.P)
                             .cornerRadius(22)
-                    }.padding(20)
+                    }
+                    .padding(20)
                 }
                 
                 
-            }.padding()
-        }.navigationBarBackButtonHidden(true)
+            }
+            .padding()
+        }
+        .navigationBarBackButtonHidden(true)
     }
     private func detectOrientation(for image: UIImage) {
         // Check image dimensions (width vs height)
@@ -85,20 +155,6 @@ struct PreviewPage: View {
             // Portrait orientation, set size to 300x600
             rotationAngle = .zero
             imageSize = CGSize(width: 350, height: 450)
-        }
-    }
-    private func updateBackgroundColor() {
-        switch value {
-        case 0...25:
-            backgroundColor = .black // Black when value is 0
-        case 26...50:
-            backgroundColor = .white // White when value is 25
-        case 51...75:
-            backgroundColor = .red // Red when value is 50
-        case 76...100:
-            backgroundColor = .blue // Blue when value is 75
-        default:
-            backgroundColor = .black // Default to black
         }
     }
 }
